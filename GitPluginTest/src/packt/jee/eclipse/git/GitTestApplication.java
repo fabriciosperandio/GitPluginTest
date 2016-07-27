fbe259446230b3ae6a455bc22def73069433a5cd
Fabrício de Reuter Sperandio <fabricio.sperandio@ifsc.edu.br>
2016-07-27 14:27:24 -0300

    ah!

--- GitPluginTest/src/packt/jee/eclipse/git/GitTestApplication.java
+++ GitPluginTest/src/packt/jee/eclipse/git/GitTestApplication.java
@@ -3,8 +3,7 @@ package packt.jee.eclipse.git;
 public class GitTestApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello world!");
 	}
 
 }
