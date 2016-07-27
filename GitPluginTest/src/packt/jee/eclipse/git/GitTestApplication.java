5563399893f9dccc449394db8f3ff70804abe666
Fabrício de Reuter Sperandio <fabricio.sperandio@ifsc.edu.br>
2016-07-27 14:30:59 -0300

    commiting to the new branch!

--- GitPluginTest/src/packt/jee/eclipse/git/GitTestApplication.java
+++ GitPluginTest/src/packt/jee/eclipse/git/GitTestApplication.java
@@ -3,7 +3,7 @@ package packt.jee.eclipse.git;
 public class GitTestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 
 }
